a01f9f27d389c8fb7197fca3a642bd7acd3adef3
nackerle <nicholas.ackerley@nrcan-rncan.gc.ca>
2023-01-20 18:23:02 +0000

    Configure numpy to use fixed-point notation.

--- src/calan/calibration_analyzer.py
+++ src/calan/calibration_analyzer.py
@@ -74,6 +74,7 @@ from calan.fit_response import fit_response, zpk_divide, zpk_out_of_band
 # %% setup
 warnings.simplefilter('error', category=BadCoefficients)
 pd.plotting.register_matplotlib_converters()
+np.set_printoptions(suppress=True)
 
 # %% defaults
 
