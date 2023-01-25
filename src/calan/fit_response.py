b4470c69b60be87c77b53ca5b737d2501df7df2a
nackerle <nicholas.ackerley@nrcan-rncan.gc.ca>
2023-01-25 14:34:43 +0000

    Still lookes like line_search is slightly better.

--- src/calan/fit_response.py
+++ src/calan/fit_response.py
@@ -201,7 +201,7 @@ def fit_response(
     gtol: float = 1e-06,
     var_lims: Tuple[float, float] = (1e-4, 0.1),
     weighting: Sequence[str] = ('variance', 'response', 'frequency'),
-    method: str = 'scipy.least_squares',
+    method: str = 'line_search',
     debug: bool = False,
 ) -> ZerosPolesGain:
     """
