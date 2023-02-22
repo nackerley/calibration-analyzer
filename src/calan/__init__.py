79445fac02b769d29c16f056b14ace5fda24dde8
nackerle <nicholas.ackerley@nrcan-rncan.gc.ca>
2023-02-22 17:22:07 +0000

    Deprecated definition of ROOT in __init__()

--- src/calan/__init__.py
+++ src/calan/__init__.py
@@ -4,5 +4,4 @@ from ._version import get_versions
 
 __version__ = get_versions()['version']  # type: ignore[attr-defined]
 PACKAGE = os.path.basename(os.path.dirname(__file__))
-ROOT = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 DATA_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data')
