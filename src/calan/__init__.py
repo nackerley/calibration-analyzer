4b42ed72c8f9978a28b2a16f08942cd52e2143ae
nackerle <nicholas.ackerley@nrcan-rncan.gc.ca>
2023-02-22 17:47:11 +0000

    Try ignoring version-related stuff in setup.cfg.

--- src/calan/__init__.py
+++ src/calan/__init__.py
@@ -2,6 +2,6 @@
 import os
 from ._version import get_versions
 
-__version__ = get_versions()['version']  # type: ignore [attr-defined]
+__version__ = get_versions()['version']
 PACKAGE = os.path.basename(os.path.dirname(__file__))
 DATA_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data')
