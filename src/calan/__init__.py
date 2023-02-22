53f0189f3514a1bf05bad748cac40a4847ee811c
nackerle <nicholas.ackerley@nrcan-rncan.gc.ca>
2023-02-22 16:59:29 +0000

    Try to eliminate Module has no attribute "get_versions"  [attr-defined].

--- src/calan/__init__.py
+++ src/calan/__init__.py
@@ -1,8 +1,8 @@
 """Initialize package."""
 import os
-from . import _version
+from ._version import get_versions
 
-__version__ = _version.get_versions()['version']
+__version__ = get_versions()['version']
 PACKAGE = os.path.basename(os.path.dirname(__file__))
 ROOT = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 DATA_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data')
