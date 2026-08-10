58a199cb1844bae92ce616dcc415661b51542fbb
Nick Ackerley <nicholas.ackerley@nrcan-rncan.gc.ca>
2026-08-10 18:22:56 +0000

    Added script to compare raw FAP results.

---
+++ studies/Yellowknife/broadband/temperature/summary.py
@@ -0,0 +1,58 @@
+"""Read raw calibration results and make summary plot."""
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.rcParams['axes.prop_cycle'] = plt.cycler(
+    color=['#777777', '#444444',  '#111111'],
+    linestyle=['-.', '--', '-'],
+)
+plt.rcParams['lines.linewidth'] = 1.0
+
+INPUT_CSV = 'arbitrary_analyzer.csv'
+data = pd.read_csv(INPUT_CSV, index_col=[0, 1])
+
+# extract temperature from filenames
+temps = (
+    data.loc[('info', 'waveform_file'), :].fillna('_NaN_')
+    .str.split('_', n=2, expand=True)[1].apply(float))
+data.columns = pd.Index(
+    ['nominal' if np.isnan(temp) else f'{temp}°C' for temp in temps])
+
+# subset to min, max, room and nominal
+keep = list(data.columns[temps.isin([
+    temps.min(),
+    temps.max(),
+    # temps[(temps - 15).abs().idxmin()],
+    np.nan])])
+data = data.loc[:, keep]
+
+magnitude_db = data.loc['magnitude_db'].astype(float)
+magnitude_db.index = pd.Index(magnitude_db.index.astype(float), name='f_hz')
+phase_deg = data.loc['phase_deg'].astype(float)
+phase_deg.index = pd.Index(phase_deg.index.astype(float), name='f_hz')
+variance_db = data.loc['variance_db'].astype(float)
+variance_db.index = pd.Index(variance_db.index.astype(float), name='f_hz')
+
+fig, axes = plt.subplots(3, 1, sharex=True, figsize=(6.5, 6.5))
+fig.subplots_adjust(hspace=0)
+mag_ax, phase_ax, var_ax = axes
+
+magnitude_db.plot(ax=mag_ax, logx=True)
+mag_ax.set_ylabel('Magnitude (dB)')
+mag_ax.set_yticks(np.arange(0, 60, 10))
+mag_ax.set_ylim(15, 55)
+phase_deg.plot(ax=phase_ax, logx=True)
+phase_ax.set_ylabel('Phase (°)')
+phase_ax.set_yticks(np.arange(0, 181, 45))
+phase_ax.set_ylim(-5, 185)
+phase_ax.get_legend().remove()
+variance_db.plot(ax=var_ax, logx=True)
+var_ax.set_ylabel('Variance (dB)')
+var_ax.set_xlabel('Frequency (Hz)')
+var_ax.set_xlim(variance_db.index.min(), variance_db.index.max())
+var_ax.get_legend().remove()
+
+fig.savefig(Path(INPUT_CSV).with_suffix('.png'), bbox_inches='tight')
